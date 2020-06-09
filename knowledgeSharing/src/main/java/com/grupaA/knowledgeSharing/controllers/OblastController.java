f224aad6d8592cf346b8379be6630cfc70df2e5e
AleksaMemedovic <aleksa51617@its.edu.rs>
2020-06-09 17:07:21 +0200

    dodao stranicu za pretragu oblasti

--- knowledgeSharing/src/main/java/com/grupaA/knowledgeSharing/controllers/OblastController.java
+++ knowledgeSharing/src/main/java/com/grupaA/knowledgeSharing/controllers/OblastController.java
@@ -4,7 +4,10 @@ import com.grupaA.knowledgeSharing.model.Oblast;
 import com.grupaA.knowledgeSharing.services.OblastService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -14,9 +17,10 @@ public class OblastController
     @Autowired
     private OblastService oblastService;
 
-    @GetMapping("/oblasti")
-    List<Oblast>all()
-    {
-        return this.oblastService.findAll();
+    @RequestMapping(value = "/oblastipretraga", method = RequestMethod.GET)
+    public String oblasti(Model model) {
+    	List<Oblast> oblasti = oblastService.findAll();
+    			model.addAttribute("oblasti", oblasti);
+    			return "oblastipretraga";
     }
 }
