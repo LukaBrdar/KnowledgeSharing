049bdf39cdd3351d5e78855402999f9871f341e9
Aleksandar <45820409+aleksandar998@users.noreply.github.com>
2020-06-14 19:50:33 +0200

    Delete IPodoblastService.java

--- knowledgeSharing/src/main/java/com/grupaA/knowledgeSharing/services/IPodoblastService.java
+++
@@ -1,9 +0,0 @@
-package com.grupaA.knowledgeSharing.services;
-
-import com.grupaA.knowledgeSharing.model.Podoblast;
-import java.util.Optional;
-
-public interface IPodoblastService
-{
-    Optional<Podoblast> findById(Long id);
-}
